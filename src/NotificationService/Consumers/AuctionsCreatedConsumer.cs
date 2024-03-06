97139693eade8db79b7f7789215e861d958ad235
duchuy200022 <duchuy200022@gmail.com>
2024-03-07 02:21:31 +0700

    feat: Add Notification service

---
+++ src/NotificationService/Consumers/AuctionsCreatedConsumer.cs
@@ -0,0 +1,24 @@
+﻿using Contracts;
+using MassTransit;
+using Microsoft.AspNetCore.SignalR;
+using NotificationService.Hubs;
+
+namespace NotificationService.Consumers
+{
+    public class AuctionsCreatedConsumer : IConsumer<AuctionCreated>
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+        public AuctionsCreatedConsumer(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task Consume(ConsumeContext<AuctionCreated> context)
+        {
+            Console.WriteLine("=>> auction created");
+
+            await _hubContext.Clients.All.SendAsync("Auction Created", context.Message);
+        }
+
+    }
+}
